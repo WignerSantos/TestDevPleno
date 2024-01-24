a93bdf16aa49674a65af91f47473bb5126cd4bf5
WignerSantos <wigner12@gmail.com>
2024-01-24 17:46:02 -0300

    Project started

---
+++ src/main/java/com/wigner/TestDevPleno/TestDevPlenoApplication.java
@@ -0,0 +1,13 @@
+package com.wigner.TestDevPleno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDevPlenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDevPlenoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wigner/TestDevPleno/TestDevPlenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wigner.TestDevPleno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevPlenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
